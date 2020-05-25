f4bf400d276d7b658005a4fd7d080374999b4770
LGC <1621853164@qq.com>
2020-05-25 14:57:20 +0800

    登录功能，增加默认账号密码判断

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.helloworld.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mBinding;
+    private String phone = "13380906674";
+    private String pwd = "123456";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +24,12 @@ public class MainActivity extends AppCompatActivity {
         mBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,HomeActivity.class);//上下文，目标activity的类
-                startActivity(intent);
+                if (mBinding.editPhone.getText().toString().equals(phone) && mBinding.editPwd.getText().toString().equals(pwd)) {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);//上下文，目标activity的类
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "密码或者手机号错误", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
