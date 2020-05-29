23ce8cd42f81f2d5bd77267117e43fcf0b3e0f62
LGC <1621853164@qq.com>
2020-05-29 10:59:59 +0800

    实现功能：将用户资料显示到Home界面

--- app/src/main/java/com/example/helloworld/HomeActivity.java
+++ app/src/main/java/com/example/helloworld/HomeActivity.java
@@ -3,12 +3,14 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.helloworld.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
     private ActivityHomeBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,17 @@ public class HomeActivity extends AppCompatActivity {
 
         Intent intent = getIntent();//获取传过来的Intent对象
         String phone = intent.getStringExtra("data_phone");//取出对应的值
-        mBinding.textPhone.setText(phone);
+        mBinding.userPhone.setText(phone);
+
+        SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
+        String userName = sp.getString("name_" + phone, "0");
+        String userSex = sp.getString("sex_" + phone, "0");
+        String userSms = sp.getString("sms_" + phone, "0").equals("1")?" 接受":" 不接受";
+
+        mBinding.userName.setText(userName);
+        mBinding.userSex.setText(userSex);
+        String temp = mBinding.userSms.getText().toString()+":"+userSms;
+        mBinding.userSms.setText(temp);
+
     }
 }
